e59404cddd33630fa9a126a8ccf41a821d3d0def
RainerAtSpirit <R.Wittmann@spirit.de>
2012-10-05 16:47:27 +0200

    Add ChrossDomainExec.js to retrieve app web and host web title

---
+++ TypeScript.Autohosted.App/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="ed1d2c0d-f9f5-4a38-be1c-9a9ee99ea9a0" featureId="ed1d2c0d-f9f5-4a38-be1c-9a9ee99ea9a0" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="TypeScript.Autohosted.App Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="44be8610-6ff6-412f-a347-d8a139fc3134" />
+  </projectItems>
+</feature>
---
+++ TypeScript.Autohosted.AppWeb/js/CrossDomainExec.js
@@ -0,0 +1,65 @@
+﻿// Load the required SharePoint libraries
+(function () {
+    var params = getParams(),
+      hostweburl = params.SPHostUrl,
+      appweburl = params.SPAppWebUrl;
+
+    // Load the js files and continue to the successHandler
+    $.getScript(hostweburl + "/_layouts/15/" + "SP.RequestExecutor.js", execCrossDomainRequest);
+
+
+    function execCrossDomainRequest() {
+        // executor: The RequestExecutor object
+        // Initialize the RequestExecutor with the app web URL.
+        var executor = new SP.RequestExecutor(appweburl);
+
+        // Retrieve information from the app web
+        executor.executeAsync(
+          {
+              url:
+                  appweburl +
+                  "/_api/web",
+              method: "GET",
+              headers: { "Accept": "application/json; odata=verbose" },
+              success: successHandler,
+              error: errorHandler
+          }
+        );
+
+        // Retrieve information from the host web
+        executor.executeAsync(
+            {
+                url:
+                    appweburl +
+                    "/_api/SP.AppContextSite(@hostweb)/web/?@hostweb='" + hostweburl + "'",
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: successHandler,
+                error: errorHandler
+            }
+        );
+
+      
+    }
+
+    function successHandler(data) {
+        var result = JSON.parse(data.body).d;
+        console.log('Title: ' + result.Title + ' Url: ' + result.Url);
+    }
+    
+    function errorHandler(data, errorCode, errorMessage) {
+        console.log ( "Could not complete cross-domain call: " + errorMessage );
+    }
+
+    // Returning the params object
+    function getParams() {
+        var params = {};
+        location.search.split('?')[1].split('&').forEach(function (param) {
+            var key = param.split('=')[0],
+                val = decodeURIComponent(param.split('=')[1]);
+            params[key] = val;
+        });
+        return params;
+    }
+
+})();
